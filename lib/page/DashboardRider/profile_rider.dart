262ec71dcee204f5c15a3e66ced1234dd054aec6
Chakhieyw <mongkonwit0075@gmail.com>
2025-10-24 23:13:24 +0700

    ใใ

--- lib/page/DashboardRider/profile_rider.dart
+++ lib/page/DashboardRider/profile_rider.dart
@@ -1,7 +1,8 @@
+import 'package:delivery_frontend/page/edit_profile_rider.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'edit_profile_rider.dart';
+
 
 class RiderProfilePage extends StatefulWidget {
   const RiderProfilePage({super.key});
